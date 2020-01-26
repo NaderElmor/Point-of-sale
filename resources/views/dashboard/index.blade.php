02a279d9e757b2805c6d5d9583df8023a532ffab
Nader Elmor <naderelmor17@gmail.com>
2020-01-26 15:08:42 +0200

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.dashboard.app')
+
+
+@section('content')
+
+    <div class="content-wrapper">
+
+        <section class="content-header">
+
+            <h1>Admin</h1>
+
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i>dashboard</li>
+            </ol>
+        </section>
+
+
+        <section class="content">
+            <h1>this is dashboard</h1>
+        </section>
+
+
+@endsection
